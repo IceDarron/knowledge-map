51ced67cac8d77b86bb3c97a1d8c38c91cc229e6
rongxuning <rongxuning@bilibili.com>
2021-02-28 15:13:35 +0800

    Initial commit

---
+++ src/main/java/com/shaman/knowledgemap/KnowledgeMapApplication.java
@@ -0,0 +1,13 @@
+package com.shaman.knowledgemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shaman/knowledgemap/KnowledgeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaman.knowledgemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
